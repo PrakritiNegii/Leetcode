cff34e3fe224197718a565cc90c7a5b75b295bd6
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2024-06-26 10:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 5.3 MB (78.71%) - LeetSync

--- 50-powx-n/powx-n.c
+++ 50-powx-n/powx-n.c
@@ -1,5 +1,5 @@
 double myPow(double x, long int n) {
-    if(x==0) return 0;
+if(x==0) return 0;
        if(n==1) return x;
        if(n>0) 
          {
@@ -10,9 +10,10 @@ double myPow(double x, long int n) {
            }
           else 
            {
-            return x * myPow(x,n-1);
+            double val =  myPow(x,n/2);
+            return x * val * val;
            }
          } 
        else if(n<0) return (1/myPow(x,-n));
-       return 1;
+       return 1;  //when n=0
 }
