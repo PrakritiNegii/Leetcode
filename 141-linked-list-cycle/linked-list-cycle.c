5a111fb4d87258703828dd0a6b256d46c6b069c5
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2024-12-11 16:02:56 +0530

    Time: 14 ms (7.23%) | Memory: 11.1 MB (53.07%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -10,6 +10,19 @@ bool hasCycle(struct ListNode *head)
  struct ListNode *fast = head, *slow = head;
  while(fast && fast->next)
   {
+   slow = slow->next;
+   fast = fast->next->next;
+   if(fast==slow)
+       return true;
+  } 
+ return false;   
+}
+/*
+bool hasCycle(struct ListNode *head) 
+{
+ struct ListNode *fast = head, *slow = head;
+ while(fast && fast->next)
+  {
    if(fast->next==slow)
        return true;
    slow = slow->next;
@@ -19,3 +32,4 @@ bool hasCycle(struct ListNode *head)
   } 
  return false;   
 }
+*/
