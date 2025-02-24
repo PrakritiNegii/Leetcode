cd08eeb2a3cb0ffee601f17a16c90f354700dcfd
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-02-24 22:08:05 +0530

    Time: 3 ms (93.11%) | Memory: 21.6 MB (89.18%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -3,14 +3,14 @@ public:
     int countCharacters(vector<string>& words, string chars) 
     {
      int count = 0;
-     vector <int> c(26,0);
+     int c[26] = {};
      for(int i=0; i<chars.size(); i++)
       {
        c[chars[i]-97]++;
       }
      for(string w : words)
       {
-       vector <int> cmp(26,0);
+       int cmp[26] = {};
        int f = 0;
        for(int i=0; i<w.size(); i++)
         {
