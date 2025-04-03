6426218e833930a00aa311bb5566977702448ebb
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-04-03 16:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.40%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,15 +1,39 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         int num = x;
+//         int n = 1;
+//         int count = 0;
+//         while(n<=num)
+//          {
+//           count++;
+//           num=num-n;
+//           n+=2;
+//          }
+//         return count;
+//     }
+// };
 class Solution {
-public:
-    int mySqrt(int x) {
-        int num = x;
-        int n = 1;
-        int count = 0;
-        while(n<=num)
-         {
-          count++;
-          num=num-n;
-          n+=2;
-         }
-        return count;
-    }
+    public:
+      int mySqrt(int x) 
+      {
+       int l = 1, r = x;
+       int res = 0;
+       while(l<=r)
+        {
+          long long mid = l + (r-l)/2;
+          if(mid*mid==x)
+            return mid;
+          else if(mid*mid < x)
+                {
+                 res = mid;
+                 l = mid+1;
+                }
+               else
+                {
+                 r = mid-1;
+                }
+        }
+       return res;
+      }
 };
