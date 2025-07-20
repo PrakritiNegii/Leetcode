b5cd9385f4baffa09a9b201ecd3ffa209507f2b2
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-07-20 14:29:57 +0530

    Time: 4 ms (11.12%) | Memory: 25.8 MB (93.35%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    int trap(vector<int>& height) 
-     {
-        int water = 0;
-        int left = 0, right = 0, min = 0;
-        int i=0, j=height.size()-1;
+    int trap(vector<int>& height) {
+       int maxh = 0;
+       int size = height.size()-1;
+       for(int i=0; i<=size; i++)
+        {
+         maxh = max(maxh,height[i]);
+        }
+       int i=0, j=size;
+       int lmax=0, rmax=0;
+       int ans=0;
 
-        while(i<=j)
-         {
-          if(left<right)
-            min = left;
-          else 
-            min = right;
-          if(height[i]>=left)
-           {
-            left = height[i];
-            i++;
-           }
-          else if(height[i]<left && left==min)
-           {
-            water = water + min - height[i];
-            i++;
-           }
-          if(height[j]>=right)
-           {
-            right = height[j];
-            j--;
-           }
-          else if(height[j]<right && right==min)
-           {
-            // if(left==min)
-            //   continue;
-            if(left!=min)
-             { 
-               water = water + min - height[j];
-               j--;
-             }
-           }
-         }
-      return water;
+       while(height[i]!=maxh)
+        {
+         if(height[i]>lmax) lmax = height[i];
+         else ans = ans + (lmax-height[i]);
+         i++;
+        } 
+       while(height[j]!=maxh)
+        {
+         if(height[j]>rmax) rmax = height[j];
+         else ans = ans + (rmax-height[j]);
+         j--;
+        }
+       while(i!=j)
+        {
+         ans = ans + (maxh - height[i]);
+         i++;
+        }
+      return ans;
     }
 };
