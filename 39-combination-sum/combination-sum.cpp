91d5553ca55803e9bf3e87551a48119eb5bfe4ed
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-07-26 08:43:39 +0530

    Time: 79 ms (11.33%) | Memory: 71.2 MB (9.10%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
-     {
-      vector<vector<int>> res;
-      generate(candidates,target,res,{},0,0);
-      return res;
-     }
     void generate(vector<int>& candidates, int target, vector<vector<int>>&res, vector<int> vec, int i, int sum)
      {
       if(sum>=target || i>=candidates.size())
@@ -18,5 +12,11 @@ public:
       generate(candidates,target,res,vec,i,sum+candidates[i]);
       vec.pop_back();
       generate(candidates,target,res,vec,i+1,sum);
+     }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+     {
+      vector<vector<int>> res;
+      generate(candidates,target,res,{},0,0);
+      return res;
      }  
 };
