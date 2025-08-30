55159c8fb5e9200325472d79f3493dc1484ae3c3
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-08-30 22:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.21%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0;
+        int _xor = 0;
+        //Finding xor of all array elements and all n whole numbers.
+        //xor of same element even times is zero
+        //Hence, the xor of all elements except the one occuring one time will be zero.
+        //So at the end xor will store the value of missing element, that is, the one that occurs only once.
         for(int i=0; i<n; i++)
          {
-          sum += nums[i];
+          _xor = _xor ^ nums[i] ^ (i+1);
          }
-        int totalSum = n*(n+1) / 2;
 
-        return totalSum - sum;
+        return _xor;
     }
 };
