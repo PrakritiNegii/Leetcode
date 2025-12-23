7ad26cc19866f54dc364869c08b346c8864e4aa9
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2025-12-23 09:39:55 +0530

    Time: 11 ms (65.38%) | Memory: 14 MB (27.20%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -6,11 +6,24 @@ public:
         int n = nums.size();
         for(int i=0; i<n; i++)
          {
+          if(i>0 && nums[i]==nums[i-1]) continue;
           int left = i+1;
           int right = n-1;
 
           while(left<right)
            {
+            if(left>i+1 && nums[left]==nums[left-1])
+             {
+              left++;
+              continue;
+             }
+
+            if(right<n-1 && nums[right]==nums[right+1])
+             {
+              right--;
+              continue;
+             }
+
             int sum = nums[i] + nums[left] + nums[right];
             int x = abs(FinalSum-target);
             int y = abs(sum-target);
@@ -22,10 +35,9 @@ public:
                   {
                     left++;
                     right--;
-                    break;
+                    return FinalSum;
                   }
            }
-          if(FinalSum==target) break;
          }
         return FinalSum;
     }
