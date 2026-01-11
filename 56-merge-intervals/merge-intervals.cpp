e451bd8426c540cb88d9846db7d22482b014f6f1
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-01-11 21:32:49 +0530

    Time: 3 ms (89.67%) | Memory: 23.9 MB (58.69%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,51 +2,27 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-
         int n = intervals.size();        
-        vector<bool> M(n,false);
         
+        sort(intervals.begin(),intervals.end());
+        for(int i=0; i<n;)
+         {
+          int start = intervals[i][0];
+          int end = intervals[i][1];
 
-    for(int x=0; x<n; x++) 
-     {
-      int start = intervals[x][0];
-      int finish = intervals[x][1];
-
-      for(int i=x+1; i<n; i++)
-        {
-          bool merged = false;
-          
-          //no change to start and finish
-          if(start<=intervals[i][0] && finish>=intervals[i][1])
-            merged = true;
-
-          //start is changed
-          if(start>=intervals[i][0] && start<=intervals[i][1]) {
-            start = intervals[i][0];
-            merged = true;
-          }
-          
-          //finish is changed
-          if(finish>=intervals[i][0] && finish<=intervals[i][1])   {
-            finish = intervals[i][1];
-            merged = true;
-           }
-          
-          if(merged==true)
+          int j=i+1;
+          for(; j<n; j++)
            {
-            intervals[i][0] = start;
-            intervals[i][1] = finish;
-            M[x] = true;
-            break;
+            if(end>=intervals[j][0])
+              end = max(end,intervals[j][1]);
+            else 
+              break;
            }
-         }
-      }
 
-      for(int i=0; i<n; i++)
-       {
-        if(M[i]==false) //didn't merge with any interval
-          ans.push_back({intervals[i][0],intervals[i][1]});
-       }
-      return ans;
+          ans.push_back({start,end});
+          i = j;
+         }
+
+        return ans;
     }
 };
