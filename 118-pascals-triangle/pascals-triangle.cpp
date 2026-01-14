061a80aab5bb73160dc28c28a202b5571bb8cfd3
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-01-14 15:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (31.61%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    long long nCr(int n, int r)
-     {
-      long long ans = 1;
-      int divisorF = min(r,n-r);
-      int i=1;
-      while(i<=divisorF)
-       {
-        ans = (ans*n)/i;
-        i++; n--;
-       }
-      return ans;
-     }
-
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> PascalTri;
         for(int i=0; i<numRows; i++)
          {
           vector<int> temp(i+1);
-          for(int j=0; j<=i; j++)
+          temp[0] = 1;
+          temp[i] = 1;
+          for(int j=1; j<i; j++)
            {
-            temp[j] = nCr(i,j);
+            temp[j] = temp[j-1] * (i-j+1) / j;
            }
           PascalTri.push_back(temp);
          }
