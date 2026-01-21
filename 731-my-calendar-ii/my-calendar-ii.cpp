e77f3b5881ea01020f60da9724384c43d83b0d25
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-01-21 21:53:23 +0530

    Time: 87 ms (47.65%) | Memory: 44.3 MB (18.71%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -0,0 +1,36 @@
+class MyCalendarTwo {
+public:
+    map<int,int> mp;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int startTime, int endTime) {
+      mp[startTime]++;
+      mp[endTime]--;
+
+      int booked = 0;
+      for(auto val : mp)
+       {
+        booked += val.second;
+
+        if(booked>2) 
+         {
+          mp[startTime]--;
+          mp[endTime]++;
+          if(mp[startTime]==0) mp.erase(startTime);
+          if(mp[endTime]==0) mp.erase(endTime);
+
+          return false;
+         }
+       }
+
+      return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
