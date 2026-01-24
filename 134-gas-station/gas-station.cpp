7076fe2ce638ef3e064335a574142f09e5e5773f
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-01-24 20:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 112.3 MB (84.85%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size();
-
-        int prevGas = 0;
-        int prevCost = 0;
-
-        int totalGas = 0;
-        int totalCost = 0;
-
-        int currGas = 0;
-
-        int ansInd = 0;
-
-        for(int i=0; i<n; i++)
-         {
-          totalGas += gas[i];
-          totalCost += cost[i];
-
-          currGas += gas[i];
-          if(cost[i]>currGas)
-           {
-            prevGas = totalGas;
-            prevCost = totalCost;
-            currGas = 0;
-            ansInd = i+1;
-           }
-          else
-           {
-            currGas = currGas - cost[i];
-           }
-         }
-
-        if(ansInd==n) return -1;
-        if((currGas+prevGas)>=prevCost) return ansInd;
-        return -1;
+        int n=gas.size();
+        int total_gas=0,total_cost=0;
+        int curr_gas=0, starting_point=0;
+        for(int i=0;i<n;i++)
+        {
+            //these two variable are to check if no case is possible
+            total_gas+=gas[i];
+            total_cost+=cost[i];
+            //for checking the total present gas at index i
+            curr_gas+=gas[i]-cost[i];
+            if(curr_gas<0)
+            {
+               //there is a breakdown....so we will start from next point or index
+                starting_point=i+1;
+                //reset our fuel 
+                //we do not need to check entire array for each starting point because if there exists an answer it will be unique and if it is not correct startingIndex value of i for it will change.
+                curr_gas=0;
+            }
+        }
+        return (total_gas<total_cost)?-1:starting_point;     
     }
 };
