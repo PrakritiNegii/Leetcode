29546f6ddabefe4698da6813e0c9ef33c2ac79b4
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-07 11:39:51 +0530

    Time: 579 ms (10.84%) | Memory: 240.1 MB (66.48%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    void compareAndMerge(vector<int>& nums, int low, int mid, int high, int &count)
+     {
+      vector<int> temp;
+      int left = low, right = mid+1;
+
+      while(left<=mid && right<=high)
+       {
+        long long l = nums[left];
+        long long r = 2LL * nums[right]; // forces the multiplication to happen in long long arithmetic, not int arithmetic.
+        if(l>r)
+         {
+          count = count + (mid-left)+1;
+          right++;
+         }
+        else left++;
+       }
+
+      left = low, right = mid+1;
+      while(left<=mid && right<=high)
+       {
+        if(nums[left]<=nums[right])
+         {
+          temp.push_back(nums[left]);
+          left++;
+         }
+        else
+         {
+          temp.push_back(nums[right]);
+          right++;
+         }
+       }
+
+       while(left<=mid)
+         {
+          temp.push_back(nums[left]);
+          left++;
+         }
+
+       while(right<=high)
+         {
+          temp.push_back(nums[right]);
+          right++;
+         }
+
+       for(int i=low, j=0; i<=high; i++,j++)
+         {
+          nums[i] = temp[j];
+         }
+       }
+
+    void divide(vector<int>& nums, int low, int high, int &count)
+     {
+      if(low>=high) return;
+      int mid = (high-low)/2 + low;
+      divide(nums,low,mid,count);
+      divide(nums,mid+1,high,count);
+      compareAndMerge(nums,low,mid,high,count);
+     }
+
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        
+        divide(nums,0,n-1,count);
+        return count;
+    }
+};
