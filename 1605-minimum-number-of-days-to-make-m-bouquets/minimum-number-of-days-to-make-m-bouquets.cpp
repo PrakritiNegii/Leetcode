d1fe221e7ee389e6190c50319bb4355fab7fcbaf
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-18 17:41:55 +0530

    Time: 24 ms (44.15%) | Memory: 70.4 MB (97.67%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+      int n = bloomDay.size();
+
+      long long totalFlowersReq = (long long) m*k;
+      if(totalFlowersReq>n) return -1;
+
+      int left = *min_element(bloomDay.begin(),bloomDay.end());
+      int right = *max_element(bloomDay.begin(),bloomDay.end());
+      int minDaysWait = left;
+
+      while(left<=right)
+       {
+        int mid = (right-left)/2 + left;
+
+        int bouquetsMade = 0;
+        int bouquetSize = 0;
+        for(int i=0; i<n; i++)
+         {
+          if(bloomDay[i]<=mid) bouquetSize++;
+          else bouquetSize = 0;
+          if(bouquetSize==k) 
+           { 
+            bouquetsMade++; 
+            bouquetSize = 0; 
+           }
+         }
+        if(bouquetsMade>=m) //valid answer, can check for shorter wait time
+         {
+          minDaysWait = mid;
+          right = mid - 1;
+         }
+        else //wait longer for flowers to bloom
+         {
+          left = mid + 1;
+         }
+       }  
+
+      return minDaysWait;
+    }
+};
