e0e20bad4f246a716e7798a23c7e2c6a3e2ca09c
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-18 20:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (28.15%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        int prevMissingCount, currMissingCount;
-        prevMissingCount = currMissingCount = arr[0]-1;
+        int left = 0;
+        int right = arr.size()-1;
 
-        int kthMissing = 0;
-
-        if(currMissingCount>=k) //In the case that kth missing no exists exists before first element of the array.
-           {
-            return k;
-           }
-
-        for(int i=0; i<n-1; i++)
+        while(left<=right)
          {
-          if(arr[i+1]>arr[i]+1) //there is a missing number
-           {
-            prevMissingCount = currMissingCount;
-            currMissingCount += arr[i+1]-arr[i]-1;
-           }
+          int mid = (right-left)/2 + left;
 
-          if(currMissingCount>=k)
-           {
-            kthMissing = arr[i] + (k-prevMissingCount);
-            break;
-           }
+          int missing = arr[mid] - mid - 1;
+
+          if(missing<k) //more missing numbers ahead
+            left = mid + 1;
+          else //find the no just before which missing nos exist (smallest no in array greater than the missing no)
+            right = mid - 1;
          }
 
-        if(kthMissing==0)
-         {
-          //There is no missing no less than max element in array
-          kthMissing = arr[n-1] + (k-currMissingCount);
-         } 
-
-        return kthMissing;
+        if(left==0) //missing no exists before first no
+          return k;
+        //left is the index after the missing number, i.e., arr[left]>missing no
+        int prevMissingNos = arr[left-1] - (left-1) - 1;
+        return arr[left-1] + (k-prevMissingNos);
     }
 };
