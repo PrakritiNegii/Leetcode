d125f1b37483575810c1665b8944811f527e8103
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-21 20:29:26 +0530

    Time: 101 ms (13.01%) | Memory: 18.8 MB (37.53%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -4,38 +4,9 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        //choosing row
-        //finding the upper bound of target in column 1
-        int row_upper=rows-1, row_lower=0;
-
-        int left=0, right=rows-1;
-        while(left<=right)
+        for(int i=0; i<rows; i++)
          {
-          int mid = (right-left)/2 + left;
-          if(matrix[mid][0]==target) return true;
-          if(target>matrix[mid][0])
-            left = mid + 1;
-          else
-            right = mid - 1;
-         }
-        row_upper = left;
-
-       left=0, right=rows-1;
-       while(left<=right)
-         {
-          int mid = (right-left)/2 + left;
-          if(matrix[mid][cols-1]==target) return true;
-          if(target>matrix[mid][cols-1])
-            left = mid + 1;
-          else
-            right = mid - 1;
-         }
-        row_lower = left;
-
-        //check columns in rows row_lower to row_upper = [row_lower,row_upper)
-        for(int i=row_lower; i<row_upper; i++)
-         {
-          left = 0, right = cols-1;
+          int left = 0, right = cols-1;
           while(left<=right)
            {
             int mid = (right-left)/2 + left;
