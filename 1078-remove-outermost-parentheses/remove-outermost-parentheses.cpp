48e7ac322466be4c62ca8559ff5c59e23ee6a011
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-23 11:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.95%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
         int n = s.size();
+        string ans = "";
 
-        int opening = 0;
+        int openingBrackets = 0;
+
         for(int i=0; i<n; i++)
          {
-          if(!opening)
+          if(openingBrackets==0) //Found first opening bracket in a decomposition
            {
-            opening++;
+            openingBrackets+=1; //Don't add the outermost bracket in the answer
            }
-          else if(s[i]=='(')
+          else if(s[i]=='(') //if openingBrackets!=0 and bracket is opening 
                 {
-                 opening++;
-                 ans += '(';
+                 ans += '('; //add bracket in answer 
+                 openingBrackets+=1; //increase the count of opening brackets
                 }
-          if(s[i]==')')
+          if(s[i]==')') //if bracket found is closing
            {
-            opening--;
-            if(!opening) continue;
-            ans += ")";
+            openingBrackets-=1; //a set of brackets is found
+            if(!openingBrackets) //set of brackets is outermost, don't add in answer
+              continue;
+            ans += ")"; //if set is not outermost add it in the answer
            }
          }
 
