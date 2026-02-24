8b1ea1c0a079c8dda77052bb75d0ba5826b6f631
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-02-24 19:46:43 +0530

    Time: 4 ms (22.90%) | Memory: 17.9 MB (37.44%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string ans = "";
-        bool odd = false;
         for(int i=num.size()-1; i>=0; i--)
          {
-          if(odd==true) 
-           {
-            ans += num[i];
-            continue;
-           }
-          int c = num[i]; //0 to 9 is 48 to 57, the corresponding ascii values are odd too
-          if(c%2!=0) 
-           {
-            odd = true;
-            ans += (char) c;
-           }
+          if(num[i]%2!=0) return num.substr(0,i+1);
          }
-
-        if(odd==false) return "";
-
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return "";
     }
 };
