354eacde20839cc37f8752011096b03cae384b18
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-07 13:49:44 +0530

    Time: 1 ms (8.98%) | Memory: 10.6 MB (61.44%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int alternateRobbing(vector<int> &nums, int i, vector<int>& dp)
-     {
-      if(i<0) return INT_MIN;
-      if(i<=1) return dp[i] = nums[i];
-
-      if(dp[i]!=-1) return dp[i];
-
-      int maxMoney = INT_MIN;
-      for(int k=2; k<=i; k++)
-       {
-        int currMoney = nums[i]; 
-        if(dp[i-k]!=-1)
-          currMoney += dp[i-k];
-        else 
-          currMoney  += alternateRobbing(nums,i-k,dp);
-        maxMoney = max(maxMoney,currMoney);
-       }
-      return dp[i] = maxMoney;
-     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        int startFirst = alternateRobbing(nums,n-1,dp);
-        int startSecond = alternateRobbing(nums,n-2,dp);
+        if(n==1) return nums[0];
 
-        return max(startFirst,startSecond);
+        vector<int> dp(n,-1);
+        dp[0] = nums[0]; dp[1] = nums[1];
+
+        for(int i=2; i<n; i++) //starting with index 0 so first jump is to index 2
+         {
+          int maxMoney = INT_MIN;
+          for(int k=2; k<=i; k++)
+           {
+            int currMoney = nums[i] + dp[i-k];
+            maxMoney = max(currMoney,maxMoney);
+           }
+          dp[i] = maxMoney;
+         }
+
+        for(int i=3; i<n; i++) //starting with index 1 so first jump is to index 3
+         {
+          int maxMoney = INT_MIN;
+          for(int k=2; k<=i; k++)
+           {
+            int currMoney = nums[i] + dp[i-k];
+            maxMoney = max(currMoney,maxMoney);
+           }
+          dp[i] = max(dp[i],maxMoney);
+         }
+
+        return max(dp[n-1],dp[n-2]);
     }
 };
