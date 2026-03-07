b69672af5aa4a3680b40b37b9e0fc9be277bbb26
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-07 17:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (52.87%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    int alternateRobbing(vector<int> &nums, int i, bool firstStart, vector<int>& dp)
-     {
-      if(i==0)
-       {
-        if(firstStart) return dp[0] = 0;
-        return dp[0] = nums[0];
-       }
-      if(i==1) return dp[i] = nums[i];
-
-      if(dp[i]!=-1) return dp[i];
-
-      int maxMoney = INT_MIN;
-      for(int k=2; k<=i; k++)
-       {
-        int currMoney = nums[i] + alternateRobbing(nums,i-k,firstStart,dp); 
-        maxMoney = max(maxMoney,currMoney);
-       }
-      return dp[i] = maxMoney;
-     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
 
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        int startFirst = alternateRobbing(nums,n-1,true,dp1);
-        int startSecond = alternateRobbing(nums,n-2,false,dp2);
-        int startThird = INT_MIN;
-        if(n>=3)
-          startThird = alternateRobbing(nums,n-3,false,dp2);
+        vector<int> dp(n,-1);
+        dp[0] = 0; dp[1] = nums[1];
 
-        int ans = max(startFirst,startSecond);
-        ans = max(ans,startThird);
+        for(int i=2; i<n; i++) //robbing the last house but not the first one
+         {
+          int maxMoney = INT_MIN;
+          for(int k=2; k<=i; k++)
+           {               
+            int currMoney = nums[i] + dp[i-k];
+            maxMoney = max(currMoney,maxMoney);
+           }
+          dp[i] = maxMoney;
+         }
 
+        vector<int> dp2(n-1,-1);
+        dp2[0] = nums[0]; dp2[1] = nums[1];
+
+        for(int i=2; i<n-1; i++) //robbing the first house but not the last one
+         {
+          int maxMoney = INT_MIN;
+          for(int k=2; k<=i; k++)
+           {               
+            int currMoney = nums[i] + dp2[i-k];
+            maxMoney = max(currMoney,maxMoney);
+           }
+          dp2[i] = maxMoney;
+         }
+
+        int ans = max(dp2[n-3],dp2[n-2]); //max money when first house was robbed from 2nd last and 3rd last houses
+        
+        ans = max(ans,dp[n-1]);
+        
         return ans;
     }
 };
