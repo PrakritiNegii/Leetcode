3bb56e40c5e0127f5bb4139b3ab5ef8bd5a72c6e
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-07 13:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.63%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int size = nums.size()-1;
-        vector<int> dp(size+1,-1);
-        int moneyA = getMoney(nums,size,dp);
-        int moneyB = INT_MIN;
-        if(size>0)
-          moneyB = getMoney(nums,size-1,dp);
-        return max(moneyA,moneyB);
-    }
-    int getMoney(vector<int>& nums, int n, vector<int>&dp)
+    int alternateRobbing(vector<int> &nums, int i, vector<int>& dp)
      {
-      if(n<=1) return nums[n];
-      if(dp[n]!=-1) return dp[n];
+      if(i<0) return INT_MIN;
+      if(i<=1) return dp[i] = nums[i];
+
+      if(dp[i]!=-1) return dp[i];
+
       int maxMoney = INT_MIN;
-      for(int i=2; i<=n; i++)
+      for(int k=2; k<=i; k++)
        {
-        cout<<"\nindex: "<<n<<"\tvalue: "<<nums[n];
-        int money = nums[n] + getMoney(nums,n-i,dp);
-        cout<<"\tmoney: "<<money;
-        maxMoney = max(money,maxMoney);
-        cout<<"\tMaxmoney: "<<maxMoney;
+
+        int currMoney = nums[i] + alternateRobbing(nums,i-k,dp); 
+        maxMoney = max(maxMoney,currMoney);
        }
-      return dp[n] = maxMoney;
+      return dp[i] = maxMoney;
      }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        int startFirst = alternateRobbing(nums,n-1,dp);
+        int startSecond = alternateRobbing(nums,n-2,dp);
+
+        return max(startFirst,startSecond);
+    }
 };
