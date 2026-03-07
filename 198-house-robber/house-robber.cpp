3040920f60c678aa00169d6d11d1b1c7e9430da5
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-07 13:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.35%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -7,18 +7,7 @@ public:
         vector<int> dp(n,-1);
         dp[0] = nums[0]; dp[1] = nums[1];
 
-        for(int i=2; i<n; i++) //starting with index 0 so first jump is to index 2
-         {
-          int maxMoney = INT_MIN;
-          for(int k=2; k<=i; k++)
-           {
-            int currMoney = nums[i] + dp[i-k];
-            maxMoney = max(currMoney,maxMoney);
-           }
-          dp[i] = maxMoney;
-         }
-
-        for(int i=3; i<n; i++) //starting with index 1 so first jump is to index 3
+        for(int i=2; i<n; i++) 
          {
           int maxMoney = INT_MIN;
           for(int k=2; k<=i; k++)
