81584732316b3629f6d345dc9a603c78da309dc9
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-09 19:00:17 +0530

    Time: 3 ms (6.15%) | Memory: 10.9 MB (5.65%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int alternateRobbing(vector<int> &nums, int i, vector<int>& dp)
-     {
-      if(i==0) return dp[i] = nums[i];
-      if(i<0) return 0;
-
-      if(dp[i]!=-1) return dp[i];
-
-      int pick = nums[i] + alternateRobbing(nums,i-2,dp);
-      int not_pick = alternateRobbing(nums,i-1,dp);
-
-      return dp[i] = max(pick,not_pick);
-     }
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n==1) return nums[0];
 
         vector<int> dp(n,-1);
-        int robbing = alternateRobbing(nums,n-1,dp);
+        int prev = nums[0]; 
+        int prev2 = 0;
 
-        return robbing;
+        for(int i=1; i<n; i++)
+         {
+          int pick = nums[i] + prev2;
+          int not_pick = prev;
+
+          prev2 = prev;
+          prev = max(pick,not_pick);
+         }
+        
+        return prev;
     }
 };
