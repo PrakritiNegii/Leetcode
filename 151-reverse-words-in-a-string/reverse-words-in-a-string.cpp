f663529b97b6b834a064e972d6cb42a6c40af3cd
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-09 21:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (69.49%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,33 +4,30 @@ public:
         int n = s.size();
         string ans = "";
 
-        stack<char> st;
-
-        int i=n; 
-        while(i>0)
+        string temp = "";
+        for(int i=n-1; i>=0; i--)
          {
-          i--;
-          if((s[i]==' ' && i==n-1) || (s[i]==' ' &&  s[i+1]==' ')) 
-            continue;
+          if(s[i]==' ' && temp.empty()) continue;
           if(s[i]==' ')
            {
-            cout<<i<<endl;
-            if(ans.size()!=0) ans += ' ';
-            while(!st.empty())
-             {
-              ans += st.top();
-              st.pop();
-             }
+            reverse(temp.begin(),temp.end());
+            
+            if(!ans.empty()) ans+=' ';
+            ans += temp;
+            temp = "";
            }
-          else 
-            st.push(s[i]);
+          else
+           {
+            temp += s[i];
+           }
          }
-        if(!st.empty() && ans.size()!=0) ans += ' ';
-        while(!st.empty())
-             {
-              ans += st.top();
-              st.pop();
-             }
+
+        reverse(temp.begin(),temp.end());
+        if(!temp.empty()) 
+         {
+          if(!ans.empty()) ans += ' ';
+          ans += temp;
+         }
         return ans;
     }
 };
