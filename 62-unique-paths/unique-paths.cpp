463a28b702ba88635557a13caf9df7a41247e59d
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-12 22:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.55%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,5 @@
 class Solution {
-public:
-    int totalUnique(int m, int n, vector<vector<int>>& dp)
-     {
-      if(n==0 && m==0) return 1;
-
-      if(dp[m][n]!=-1) return dp[m][n];
-
-      //Approach is top down starting from m-1, n-1
-      int top=0, left=0;
-      if(m>0)
-        top = totalUnique(m-1,n,dp);
-      if(n>0)
-        left = totalUnique(m,n-1,dp);
-
-      return dp[m][n] = top+left;
-     }
-
+public: 
     int uniquePaths(int m, int n) {
          vector<vector<int>> dp(m,vector<int>(n,0));
          
