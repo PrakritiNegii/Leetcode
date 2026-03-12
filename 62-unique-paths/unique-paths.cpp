bab3e9dc33118f09cbf1d9c4e9c94c7a688a0158
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-12 17:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (51.55%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,8 +17,23 @@ public:
      }
 
     int uniquePaths(int m, int n) {
-         vector<vector<int>> dp(m,vector<int>(n,-1));
+         vector<vector<int>> dp(m,vector<int>(n,0));
+         
+         dp[0][0] = 1;
 
-         return totalUnique(m-1,n-1,dp);
+         for(int i=0; i<m; i++)
+          {
+           for(int j=0; j<n; j++)
+            {
+             //check can you reach this particular cell from the top or from the left
+             int newr = i-1; //check top
+             int newc = j-1; //check left
+
+             if(newr>=0) dp[i][j] += dp[newr][j];
+             if(newc>=0) dp[i][j] += dp[i][newc];
+            }
+           cout<<endl;
+          }
+         return dp[m-1][n-1];
     }
 };
