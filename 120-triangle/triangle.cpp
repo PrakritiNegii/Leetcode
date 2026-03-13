ed933404b900949ac0a45e8b8ce3bd8b49107b0d
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-13 16:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (23.40%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int triangleMinPath(vector<vector<int>> & triangle, int i, int j, vector<vector<int>>& dp)
      {
-      if(j<0 || j>=triangle[i].size()) return INT_MAX;
       if(i==0 && j==0) return triangle[i][j]; //reached peak (first element)
       if(dp[i][j]!=-1) return dp[i][j];
 
       int sameCol = INT_MAX, lesserCol = INT_MAX;
 
-      int val = triangleMinPath(triangle,i-1,j,dp);
-      if(val==INT_MAX) sameCol = INT_MAX;
-      else sameCol = triangle[i][j] + triangleMinPath(triangle,i-1,j,dp);
+      if(j<triangle[i].size()-1)
+       sameCol = triangle[i][j] + triangleMinPath(triangle,i-1,j,dp);
 
-      val = triangleMinPath(triangle,i-1,j-1,dp);
-      if(val==INT_MAX) lesserCol = INT_MAX;
-      else lesserCol = triangle[i][j] + triangleMinPath(triangle,i-1,j-1,dp);
+      if(j>0)
+       lesserCol = triangle[i][j] + triangleMinPath(triangle,i-1,j-1,dp);
 
       return dp[i][j] = min(sameCol,lesserCol);
      }
