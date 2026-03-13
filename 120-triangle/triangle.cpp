91490764aeda4dfa77805fad3847774d9edb9caa
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-13 17:03:20 +0530

    Time: 4 ms (6.57%) | Memory: 13.1 MB (56.45%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    int triangleMinPath(vector<vector<int>> & triangle, int i, int j, vector<vector<int>>& dp)
-     {
-      if(i==0 && j==0) return triangle[i][j]; //reached peak (first element)
-      if(dp[i][j]!=-1) return dp[i][j];
-
-      int sameCol = INT_MAX, lesserCol = INT_MAX;
-
-      if(j<triangle[i].size()-1)
-       sameCol = triangle[i][j] + triangleMinPath(triangle,i-1,j,dp);
-
-      if(j>0)
-       lesserCol = triangle[i][j] + triangleMinPath(triangle,i-1,j-1,dp);
-
-      return dp[i][j] = min(sameCol,lesserCol);
-     }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int base = triangle[m-1].size();
 
-        vector<vector<int>> dp(m,vector<int>(base,-1));
+        vector<int>prev(base,0);
 
-        int minPath = INT_MAX;
-        for(int i=0; i<base; i++)
+        for(int i=0; i<m; i++)
          {
-          minPath = min(minPath,triangleMinPath(triangle,m-1,i,dp));
+          int n = triangle[i].size();
+          vector<int> curr(base,0);
+
+          for(int j=0; j<n; j++)
+           {
+            if(i==0 && j==0) 
+             {
+              curr[j] = triangle[i][j];
+              continue;
+             }
+
+            int sameCol = INT_MAX, lesserCol=INT_MAX;
+            
+            if(j<n-1)
+             sameCol = triangle[i][j] + prev[j];
+            if(j>0)
+             lesserCol = triangle[i][j] + prev[j-1];
+
+            curr[j] = min(sameCol,lesserCol);
+           }
+
+          prev = curr;
          }
+
+        int minPath = prev[0];
+        for(int i=1; i<base; i++)
+          minPath =  min(minPath,prev[i]);
 
         return minPath;
     }
