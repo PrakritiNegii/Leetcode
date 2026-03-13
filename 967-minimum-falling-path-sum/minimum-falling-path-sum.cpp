428b85deb344dd36d51580f24aff05e1a402395a
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-13 20:41:47 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (42.76%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,33 +4,35 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> prev(n,0);
         for(int j=0; j<n; j++)
          {
-          dp[0][j] = matrix[0][j];
+          prev[j] = matrix[0][j];
          }
 
         for(int i=1; i<m; i++)
          {
+          vector<int> curr(n,0);
           for(int j=0; j<n; j++)
            {
             int below = INT_MAX, diagonalLeft = INT_MAX, diagonalRight = INT_MAX;
 
-            below = matrix[i][j] + dp[i-1][j];
+            below = matrix[i][j] + prev[j];
             if(j>0) 
-              diagonalLeft = matrix[i][j] + dp[i-1][j-1];
+              diagonalLeft = matrix[i][j] + prev[j-1];
             if(j<n-1) 
-              diagonalRight = matrix[i][j] + dp[i-1][j+1];
+              diagonalRight = matrix[i][j] + prev[j+1];
 
-            dp[i][j] = min({below,diagonalLeft,diagonalRight});
+            curr[j] = min({below,diagonalLeft,diagonalRight});
            }
+          prev = curr;
          }
 
         int minP = INT_MAX;
 
         for(int j=0; j<n; j++)
          {
-          minP = min(minP, dp[m-1][j]);
+          minP = min(minP, prev[j]);
          }
 
         return minP;
