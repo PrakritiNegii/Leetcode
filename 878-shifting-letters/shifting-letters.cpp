d019e29b794115e81754e188fd202d2a732fe334
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-14 10:05:41 +0530

    Time: 3 ms (72.63%) | Memory: 71.6 MB (84.00%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+      int n = s.size();
+
+      int toShift = 0;
+      for(int i=0; i<n; i++)
+       {
+        shifts[i] = shifts[i]%26;
+        toShift += shifts[i];
+       }  
+   
+      for(int i=0; i<n; i++)
+       {
+        int shift = toShift % 26;
+        int ch = ((s[i]-97) + shift) % 26;
+        s[i] = (char) ch+97;
+
+        toShift -= shifts[i];
+       }
+      cout<<s<<endl;
+      return s;
+    }
+};
