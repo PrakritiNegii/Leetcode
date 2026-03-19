f636d85ef5f501f81bd0f946df9e29fad5c9af27
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-19 18:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (68.60%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int totalDecodings(string &s, int i, vector<int>& dp)
-     {
-      if(s[i]=='0') return 0;
-
-      if(i>=s.size()-1)
-       {
-         return 1;
-       }
-
-      if(dp[i]!=-1)
-          return dp[i];
-         
-      int count = 0;
-
-      count += totalDecodings(s,i+1,dp); //current single digit is valid go to next
-
-      int l = s[i]-48, r = s[i+1]-48;
-      int val = l*10 + r;
-
-      if(val>=1 && val<=26) //current two digit no is valid go to next
-        count+= totalDecodings(s,i+2,dp);
-      
-      return dp[i] = count;
-     }
-
     int numDecodings(string s) {
       int n = s.size();
-      vector<int> dp(n,-1);
-      return totalDecodings(s,0,dp);
+
+      vector<int> dp(n+1,0);
+      dp[n] = 1; //if we reach the end then the string has a valid decoding
+
+      for(int i=n-1; i>=0; i--)
+       {
+        if(s[i]=='0') continue;
+        dp[i] += dp[i+1];
+        if(i < n-1) //another character present after the current
+          {
+            int l = s[i]-48, r = s[i+1]-48;
+            int val = l*10 + r;
+            if(val<=26)
+              dp[i] += dp[i+2];
+          }
+       }
+      return dp[0];
     }
 };
