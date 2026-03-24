6eb8666c4331f18a1b721d6831516ae1e1d4a140
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-24 20:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.56%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        int base = triangle[m-1].size();
+        int n = triangle.size();
 
-        vector<int>prev(base,0);
+        vector<int> dp = triangle[n-1];  // start from last row
 
-        for(int i=0; i<m; i++)
-         {
-          int n = triangle[i].size();
-          vector<int> curr(n,0);
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+            }
+        }
 
-          for(int j=0; j<n; j++)
-           {
-            if(i==0 && j==0) 
-             {
-              curr[j] = triangle[i][j];
-              continue;
-             }
-
-            int sameCol = INT_MAX, lesserCol=INT_MAX;
-            
-            if(j<n-1)
-             sameCol = triangle[i][j] + prev[j];
-            if(j>0)
-             lesserCol = triangle[i][j] + prev[j-1];
-
-            curr[j] = min(sameCol,lesserCol);
-           }
-
-          prev = curr;
-         }
-
-        int minPath = prev[0];
-        for(int i=1; i<base; i++)
-          minPath =  min(minPath,prev[i]);
-
-        return minPath;
+        return dp[0];
     }
 };
