81fd18cf941037ae665a157dd3a1311b6bab24da
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-25 21:00:51 +0530

    Time: 6 ms (75.90%) | Memory: 126.5 MB (85.61%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        unordered_set<long long> horizontal;
-        unordered_set<long long> vertical;
+        long long total = 0LL, h = 0LL, v = 0LL;
+        
+        for(int i=0; i<grid.size(); i++)
+         {
+          for(int j=0; j<grid[0].size(); j++)
+           {
+            total += grid[i][j];
+           }
+         }
 
-        long long h = 0LL, v = 0LL;
+        long double half_total = total / 2.0;
 
         for(int i=0; i<grid.size(); i++)
          {
@@ -12,7 +19,7 @@ public:
            {
             h += grid[i][j];
            }
-          horizontal.insert({h});
+          if(h == half_total) return true;
          }
 
         for(int j=0; j<grid[0].size(); j++)
@@ -21,23 +28,7 @@ public:
            {
             v += grid[i][j];
            }
-          vertical.insert({v});
-         }
-
-        long double half_h = h/2.0;
-        long double half_v = v/2.0;
-
-        long long hh = h/2;
-        long long hv = v/2;
-
-        if(half_h == hh)
-         {
-          if(horizontal.find(hh) != horizontal.end()) return true;
-         }
-
-        if(half_v == hv)
-         {
-          if(vertical.find(hv) != vertical.end()) return true;
+          if(v == half_total) return true;
          }
 
         return false;
