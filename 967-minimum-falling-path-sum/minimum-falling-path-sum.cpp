b667a32309562ecd2b2d775cef380561c560a726
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-26 15:33:02 +0530

    Time: 9 ms (6.05%) | Memory: 14.9 MB (61.86%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        vector<int> prev(n,0);
-        for(int j=0; j<n; j++)
+        vector<int> prev(m,0);
+        for(int j=0; j<m; j++)
          {
           prev[j] = matrix[0][j];
          }
 
-        for(int i=1; i<m; i++)
+        for(int i=1; i<n; i++)
          {
-          vector<int> curr(n,0);
-          for(int j=0; j<n; j++)
+          vector<int> curr(m,0);
+          for(int j=0; j<m; j++)
            {
             int below = INT_MAX, diagonalLeft = INT_MAX, diagonalRight = INT_MAX;
 
             below = matrix[i][j] + prev[j];
             if(j>0) 
               diagonalLeft = matrix[i][j] + prev[j-1];
-            if(j<n-1) 
+            if(j<m-1) 
               diagonalRight = matrix[i][j] + prev[j+1];
 
             curr[j] = min({below,diagonalLeft,diagonalRight});
@@ -30,7 +30,7 @@ public:
 
         int minP = INT_MAX;
 
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
          {
           minP = min(minP, prev[j]);
          }
