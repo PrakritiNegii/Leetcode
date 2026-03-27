8f2030b1da9f54b499212a9b69a35e30153ed68f
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-03-27 19:34:40 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (67.70%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -4,37 +4,11 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> temp(n);
-
         for(int i=0; i<n; i++)
          {
           for(int j=0; j<m; j++)
            {
-            temp[i].push_back(mat[i][j]);
-           }
-         }
-
-        k = k % m;
-
-        for(int i=0; i<n; i++)
-         {
-          if(i%2==0) //left shift
-           {
-            reverse(temp[i].begin(), temp[i].begin()+k);
-            reverse(temp[i].begin()+k, temp[i].end());
-            reverse(temp[i].begin(), temp[i].end());
-           }
-          else //right shift
-           {
-            int rightShift = m - k;
-            reverse(temp[i].begin(), temp[i].begin()+rightShift);
-            reverse(temp[i].begin()+rightShift, temp[i].end());
-            reverse(temp[i].begin(), temp[i].end());
-           }
-
-          for(int j=0; j<m; j++)
-           {
-            if(mat[i][j] != temp[i][j]) return false;
+            if(mat[i][(j+k)%m] != mat[i][j]) return false;
            }
          }
 
