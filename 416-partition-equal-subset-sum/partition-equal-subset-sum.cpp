a7385c6889b3e17d520c6f10d5532ab2e87ab632
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-03 11:42:12 +0530

    Time: 175 ms (20.08%) | Memory: 17.4 MB (59.06%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    bool partitioning(vector<int>& nums, int i, int sum, vector<vector<int>>& dp)
-     {
-      if(sum<0) return false;
-      if(sum==0) return true;
-      if(i==0) return false;
-
-      if(dp[i][sum]!=-1) return dp[i][sum];
-      //picking for subset 1;
-      bool s1 = partitioning(nums,i-1,sum-nums[i],dp);
-
-      //picking for subset 2;
-      bool s2 = partitioning(nums,i-1,sum,dp);
-
-      return dp[i][sum] = s1 || s2;
-     }
-
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int total = 0;
+
         for(int i=0; i<n; i++)
          {
           total += nums[i];
@@ -27,9 +12,31 @@ public:
         if(total%2==1) return false;
         
         total/=2;
+        vector<vector<bool>> dp(n, vector<bool>(total+1,false));
+        for(int i=0; i<n; i++)
+         {
+          dp[i][0] = true;
+         }
+        if(nums[0] <= total)
+          dp[0][nums[0]] = true;
 
-        vector<vector<int>> dp(n, vector<int>(total+1,-1));
+        for(int i=1; i<n; i++)
+         {
+          for(int j=1; j<=total; j++)
+           {
+            bool s1 = false;
 
-        return partitioning(nums,n-1,total,dp);
+            //picking for subset 1;
+            if(nums[i]<=j)
+              s1 = dp[i-1][j-nums[i]];
+
+            //picking for subset 2 or not picking for subset 1;
+            bool s2 = dp[i-1][j];
+
+            dp[i][j] = s1 || s2;
+           }
+         }
+
+        return dp[n-1][total];
     }
 };
