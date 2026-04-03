02e12e3bec1b687541bdb9432a5246d142bc4873
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-03 11:50:33 +0530

    Time: 107 ms (67.05%) | Memory: 16.7 MB (71.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -12,31 +12,33 @@ public:
         if(total%2==1) return false;
         
         total/=2;
-        vector<vector<bool>> dp(n, vector<bool>(total+1,false));
-        for(int i=0; i<n; i++)
-         {
-          dp[i][0] = true;
-         }
+
+        vector<bool> prev(total+1,false);
+        prev[0] = true;
         if(nums[0] <= total)
-          dp[0][nums[0]] = true;
+          prev[nums[0]] = true;
 
         for(int i=1; i<n; i++)
          {
+          vector<bool> curr(total+1,false);
+          curr[0] = true;
+
           for(int j=1; j<=total; j++)
            {
             bool s1 = false;
 
             //picking for subset 1;
             if(nums[i]<=j)
-              s1 = dp[i-1][j-nums[i]];
+              s1 = prev[j-nums[i]];
 
             //picking for subset 2 or not picking for subset 1;
-            bool s2 = dp[i-1][j];
+            bool s2 = prev[j];
 
-            dp[i][j] = s1 || s2;
+            curr[j] = s1 || s2;
            }
+          prev = curr;
          }
 
-        return dp[n-1][total];
+        return prev[total];
     }
 };
