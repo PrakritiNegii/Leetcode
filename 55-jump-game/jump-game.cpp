d17f30693d65ff9a8cb295da8732b54dac83e26d
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-12 15:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (24.24%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,29 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return true;
-        if(nums[0]==0) return false;
-        unordered_set<int> zeroes;
-        for(int i=0; i<n; i++)
-         {
-          if(nums[i]==0) zeroes.insert(i);
-         }
-
-        if(zeroes.size()==0) return true;
+        int validIdx = n-1;
 
         for(int i=n-2; i>=0; i--)
          {
-          if(nums[i]==0) continue;
-          int j = i+nums[i];
-          if(j>=n-1) continue;
-          while(j>i && zeroes.count(j)!=0)
-           {
-            j--;
-           }
-          if(j==i) zeroes.insert(j);
+          if(nums[i]>= (validIdx-i)) validIdx = i;
          }
 
-        if(zeroes.count(0)!=0) return false;
-      return true;
+        if(validIdx!=0) return false;
+        return true;
     }
 };
