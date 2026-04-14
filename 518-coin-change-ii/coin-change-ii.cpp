8280a634175104185b166f9dd7b82d31dccd48c6
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-14 11:43:37 +0530

    Time: 23 ms (70.45%) | Memory: 10.8 MB (72.19%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -17,7 +17,27 @@ public:
     int change(int amount, vector<int>& coins) {
        int n = coins.size();
 
-       vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-       return allWays(coins,n-1,amount,dp); 
+       vector<long long> prev(amount+1,0), curr(amount+1,0); 
+        prev[0] = 1;
+        curr[0] = 1;
+
+        for(int i=0; i<n; i++)
+         {
+          for(int amt=1; amt<=amount; amt++)
+           {
+            int notTake = 0;
+            if(i>0)
+              notTake = prev[amt];
+
+            int take = 0;
+            if(amt>=coins[i])
+               take = curr[amt-coins[i]];
+
+            curr[amt] = (long long) notTake + take;
+           }
+          prev = curr;
+         }
+
+       return (int) prev[amount]; 
     }
 };
