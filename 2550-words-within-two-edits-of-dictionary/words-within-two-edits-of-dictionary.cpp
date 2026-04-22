897a4649a7e1fe06e8e5f68665ad2e3913353c17
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-22 21:19:34 +0530

    Time: 11 ms (49.20%) | Memory: 18.9 MB (37.43%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int sizeQ = queries.size();
+        int sizeD = dictionary.size();
+        int n = queries[0].size();
+
+        vector<string> ans;
+
+        for(int i=0; i<sizeQ; i++)
+         {
+          string q = queries[i];
+          for(int j=0; j<sizeD; j++)
+           {
+            string d = dictionary[j];
+            if(q==d) 
+             {
+              ans.push_back(q);
+              break; //one match is enough to push the query
+             }
+            int edits = 0;
+            for(int k=0; k<n; k++)
+             {
+              if(q[k]!=d[k]) edits++;
+              if(edits>2) break;
+             }
+            if(edits<=2) 
+             {
+              ans.push_back(q);
+              break; //one match is enough to push the query
+             }
+           }
+         }
+
+        return ans;
+    }
+};
