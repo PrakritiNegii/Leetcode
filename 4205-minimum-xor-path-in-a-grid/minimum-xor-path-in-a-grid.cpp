e4c759310c35ef64ce6734a048239e55eba27535
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-22 09:20:57 +0530

    Time: 887 ms (21.82%) | Memory: 364.3 MB (47.44%) - LeetSync

--- 4205-minimum-xor-path-in-a-grid/minimum-xor-path-in-a-grid.cpp
+++ 4205-minimum-xor-path-in-a-grid/minimum-xor-path-in-a-grid.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int minXorCost(vector<vector<int>>& grid, int r, int c, int xorVal, vector<vector<vector<int>>>& dp)
-     {
-      if(r==0 && c==0)
-        return xorVal ^ grid[0][0];
-      if(dp[r][c][xorVal]!=-1) return dp[r][c][xorVal];
-
-      int up = 1023, left = 1023;
-      int currXor = xorVal ^ grid[r][c];
-
-      if(r>0)
-        up = minXorCost(grid,r-1,c,currXor,dp);
-      if(c>0)
-        left = minXorCost(grid,r,c-1,currXor,dp);
-
-      return dp[r][c][xorVal] = min(up,left);      
-     }
-
     int minCost(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m, vector<int>(1024,-1))); 
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m, vector<int>(1024,0))); 
         //range of xor values is taken as 1024 as the max value in grid can be 1023
         //The max no of 1s is 10 no xor can add extra 1
+
+        for(int r=0; r<n; r++)
+         {
+          for(int c=0; c<m; c++)
+           {
+            for(int xorVal=0; xorVal<1024; xorVal++)
+             {
+              if(r==0 && c==0)
+               {
+                dp[0][0][xorVal] = xorVal ^ grid[0][0];
+                continue;
+               }
+              int up = 1023, left = 1023;
+              int currXor = xorVal ^ grid[r][c];
+
+              if(r>0)
+                up = dp[r-1][c][currXor];
+              if(c>0)
+                left = dp[r][c-1][currXor];
+
+              dp[r][c][xorVal] = min(up,left);
+             }
+           }
+         }
         
-        return minXorCost(grid,n-1,m-1,0,dp);
+        return dp[n-1][m-1][0];
     }
 };
