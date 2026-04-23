38fd651d11c03c04705528ad707214e6e9886efc
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-23 22:04:31 +0530

    Time: 81 ms (44.50%) | Memory: 116.1 MB (69.00%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map <int, vector<int> > mp;
+      
+        for(int i=0; i<n; i++)
+         {
+          int val = nums[i];
+          mp[val].push_back(i);
+         }
+
+        // for(auto val : mp)
+        //  {
+        //   cout<<"\t"<<val.first<<endl;
+        //   int size = val.second.size();
+        //   for(int i=0; i<size; i++)
+        //      cout<<val.second[i]<<endl;
+        //  }
+        //  cout<<"------------------------"<<endl;
+
+        vector<long long> count(n,0);
+
+        for(auto val : mp)
+         {
+          int size = val.second.size();
+          long long left = 0LL, right = 0LL;
+          for(int j=1; j<size; j++)
+            right += (long long) val.second[j];
+
+          int leftSize = 0, rightSize = size-1;
+          for(int j=0; j<size; j++)
+           {
+            int currPos = val.second[j];
+            // cout<<currPos<<endl;
+            // cout<<leftSize<<"\t"<<rightSize<<endl;
+            // cout<<left<<"\t"<<right<<endl<<endl<<endl;
+            count[currPos] += abs((long long) currPos*leftSize - left);
+            count[currPos] += abs((long long) currPos*rightSize - right);
+
+            left += val.second[j];
+            if(j<size-1) right -= val.second[j+1];
+
+            leftSize++; rightSize--;
+           }
+         }
+        return count;
+    }
+};
