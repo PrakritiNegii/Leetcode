d8a26e069c6456c5dfc8b3bac3c6ce43b074aad9
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-24 21:49:56 +0530

    Time: 130 ms (38.01%) | Memory: 209.7 MB (19.29%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    bool isParent(string parent, int newr, int newc)
-     {
-      string curr = to_string(newr) + ',' + to_string(newc);
-
-      return (parent==curr);
-     }
-
     bool hasCycle(vector<vector<char>>& grid, int si, int sj, vector<vector<char>>& visited)
      {
        int n = grid.size();
        int m = grid[0].size();
 
        char ch = grid[si][sj];
-       queue<pair<pair<int,int>,string>> q;
-       q.push({{si,sj},"-1,-1"});
+       queue<pair<pair<int,int>,pair<int,int>>> q;
+       q.push({{si,sj},{-1,-1}});
 
        while(!q.empty())
         {
          int r = q.front().first.first;
          int c = q.front().first.second;
-         string parent = q.front().second;
-         string current = to_string(r) + ',' + to_string(c);
+         int rParent = q.front().second.first;
+         int cParent = q.front().second.second;
 
          q.pop();
 
@@ -33,15 +26,15 @@ public:
            int newr = r + rowD[k];
            int newc = c + colD[k];
 
-           bool isThisParent = isParent(parent,newr,newc);
-           if(isThisParent) continue;
+           if(newr==rParent && newc==cParent) 
+              continue;
 
            if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==ch)
             {
              if(visited[newr][newc]=='0')
               {
                 visited[newr][newc] = '1';
-                q.push({{newr,newc},current});
+                q.push({{newr,newc},{r,c}});
               }
              else
                 return true;
