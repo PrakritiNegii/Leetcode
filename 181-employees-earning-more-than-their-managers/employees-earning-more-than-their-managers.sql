9586676ccc8e008b471c698c97abb82dadb858fa
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-24 18:55:03 +0530

    Time: 405 ms (26.81%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -3,7 +3,7 @@
 -- FROM Employee WHERE salary > (SELECT salary FROM Employee WHERE id=managerId);
 -- don't use managerId = Null, Because NULL means unknown, SQL uses three-valued logic: True, False anmd Unknown
 
-SELECT e1.name as Employee FROM Employee e1 WHERE e1.managerId IS NOT NULL
-AND e1.salary > (
-    SELECT e2.salary FROM Employee e2 WHERE e2.id = e1.managerId
-    );  
+    SELECT e1.name as Employee FROM Employee e1
+    LEFT JOIN Employee e2 
+    ON e1.managerId = e2.id 
+    WHERE e1.salary > e2.salary;
