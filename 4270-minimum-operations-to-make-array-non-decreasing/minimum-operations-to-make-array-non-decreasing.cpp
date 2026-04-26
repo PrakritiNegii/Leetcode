3520b0fffc468eb53cfb42e638086bff044380eb
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-26 11:38:39 +0530

    Time: 5 ms (28.88%) | Memory: 137 MB (34.15%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
@@ -3,19 +3,11 @@ public:
     long long minOperations(vector<int>& nums) {
        int n = nums.size();
 
-        int mini = INT_MAX;
         long long allx = 0;
-        for(int i=n-1; i>=0; i--)
+        for(int i=n-2; i>=0; i--)
             {
-              if(nums[i]>mini) 
-               {
-                allx += (nums[i]-mini);
-                mini = nums[i];
-               }
-              else
-               {
-                mini = min(mini,nums[i]);
-               }              
+              if(nums[i]>nums[i+1]) 
+                allx += (nums[i] - nums[i+1]);
             }
         return allx;
     }
