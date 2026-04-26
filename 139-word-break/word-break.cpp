8dc84ff04ee310182e786fb4b2d94420ae81e46b
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-26 22:16:04 +0530

    Time: 34 ms (5.04%) | Memory: 29.9 MB (5.19%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-       vector<int> dp(s.size(),-1);
-       return find(s,wordDict,0,dp);
-    }
-
-    bool find(string s, vector<string>& wordDict, int pos, vector<int>&dp)
+    bool breakable(string &s, unordered_set<string>& st, int i, string segment, vector<int>& dp)
      {
-      if(pos>=s.size())
-        return true;
-      if(dp[pos]!=-1) 
-        return dp[pos];
-
-      for(int k=0; k<wordDict.size(); k++)
+      if(i==s.size()-1)
        {
-        if(s[pos]==wordDict[k][0])
-         {
-          int p = pos+1;
-          int c = 1;
-          bool flag = true;
-          while(c<wordDict[k].size())
-           {
-            if(p>=s.size() || s[p]!=wordDict[k][c])
-              {
-                flag = false;
-                break;
-              }
-            p++;
-            c++;
-           }
-          if(flag==true)
-           {
-            bool res = find(s,wordDict,p,dp);
-            if(res==true) 
-              {
-                dp[pos] = 1;
-                return res;
-              }
-            else
-              {
-                dp[pos] = 0;
-              }
-           }
-         }
+        segment += s[i];
+        if(st.count(segment)!=0) return true;
+        return false;
        }
-     return false;
+      
+      segment += s[i];
+      bool breaks1 = breakable(s,st,i+1,segment,dp);
+      if(breaks1) return true;
+
+      bool breaks2 = false;
+      if(st.count(segment)!=0)
+        {
+         if(dp[i]!=-1) return dp[i];
+         breaks2 = breakable(s,st,i+1,"",dp);
+         dp[i] = breaks2;
+        }
+      
+      return breaks2;
      }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        unordered_set <string> st;
+        vector<int> dp(n,-1);
+
+        for(string str : wordDict)
+           st.insert(str);
+
+        return breakable(s,st,0,"",dp);
+    }
 };
