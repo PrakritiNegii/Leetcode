b7c05f0edd23d41219b245e20910edbc30db658b
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-28 08:28:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (72.30%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool breakable(string &s, unordered_set<string>& st, int i, string segment, vector<int>& dp)
+    bool breakable(string &s, unordered_set<string>& st, int i, vector<int>& dp)
      {
-      if(i==s.size()-1)
+      if(i>=s.size()) return true;
+      if(dp[i]!=-1) return dp[i];
+      
+      string segment = "";
+      bool canBreak = false;
+      for(int j=i; j<s.size(); j++)
        {
-        segment += s[i];
-        if(st.count(segment)!=0) return true;
-        return false;
-       }
-      
-      segment += s[i];
-      bool breaks1 = breakable(s,st,i+1,segment,dp);
-      if(breaks1) return true;
+        segment += s[j];
+        if(st.count(segment)!=0)
+           canBreak = breakable(s,st,j+1,dp);
 
-      bool breaks2 = false;
-      if(st.count(segment)!=0)
-        {
-         if(dp[i]!=-1) return dp[i];
-         breaks2 = breakable(s,st,i+1,"",dp);
-         dp[i] = breaks2;
-        }
-      
-      return breaks2;
+        if(canBreak) return dp[j+1] = true;
+       }
+      return dp[i] = false;
      }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         unordered_set <string> st;
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,-1);
 
         for(string str : wordDict)
            st.insert(str);
 
-        return breakable(s,st,0,"",dp);
+        return breakable(s,st,0,dp);
     }
 };
