405a710d5fecc6be979464a89e7fe6f2d016cd48
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-28 08:47:38 +0530

    Time: 6 ms (60.19%) | Memory: 10.8 MB (80.82%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    bool breakable(string &s, unordered_set<string>& st, int i, vector<int>& dp)
-     {
-      if(i>=s.size()) return true;
-      if(dp[i]!=-1) return dp[i];
-
-      string segment = "";
-      bool canBreak = false;
-      for(int j=i; j<s.size(); j++)
-       {
-        segment += s[j];
-        if(st.count(segment)!=0)
-           canBreak = breakable(s,st,j+1,dp);
-
-        if(canBreak) return dp[i] = true;
-       }
-      return dp[i] = false;
-     }
-
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         unordered_set <string> st{begin(wordDict), end(wordDict)};
-        vector<int> dp(n,-1);
-        
-        return breakable(s,st,0,dp);
+        vector<bool> dp(n+1,false);
+        dp[n] = true;
+
+        for(int i=n-1; i>=0; i--)
+         {
+           string segment = "";
+           bool canBreak = false;
+           for(int j=i; j<s.size(); j++)
+            {
+             segment += s[j];
+             if(st.count(segment)!=0)
+               dp[i] = dp[j+1];
+             if(dp[i]) break;
+            }
+         }
+        for(int i=0; i<n; i++)
+          cout<<dp[i]<<endl;
+
+        return dp[0];
     }
 };
