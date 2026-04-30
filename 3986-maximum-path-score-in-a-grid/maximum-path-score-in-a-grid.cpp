208e7f6a982b67359bf5c80215228422e37f5d20
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-04-30 14:51:26 +0530

    Time: 944 ms (30.67%) | Memory: 100.9 MB (91.64%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -4,11 +4,11 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k+1,-1e8)));
+        vector<vector<int>> prev(m,vector<int>(k+1,-1e8)), curr(m,vector<int>(k+1,-1e8));
         for(int cost=0; cost<=k; cost++)
          {
-          if(grid[0][0]==0) dp[0][0][cost] = 0;
-          else if(cost>0) dp[0][0][cost] = grid[0][0]; 
+          if(grid[0][0]==0) prev[0][cost] = curr[0][cost] = 0;
+          else if(cost>0) prev[0][cost] = curr[0][cost] = grid[0][0]; 
          }
 
         for(int i=0; i<n; i++)
@@ -21,20 +21,21 @@ public:
               int up = -1e8, left = -1e8;
               if(i>0)
                {
-                if(grid[i][j]==0) up = dp[i-1][j][cost];
-                else if(cost>0) up = grid[i][j] + dp[i-1][j][cost-1];
+                if(grid[i][j]==0) up = prev[j][cost];
+                else if(cost>0) up = grid[i][j] + prev[j][cost-1];
                }
               if(j>0)   
                {
-                if(grid[i][j]==0) left = dp[i][j-1][cost];
-                else if(cost>0) left = grid[i][j] + dp[i][j-1][cost-1];
+                if(grid[i][j]==0) left = curr[j-1][cost];
+                else if(cost>0) left = grid[i][j] + curr[j-1][cost-1];
                }
-              dp[i][j][cost] = max(up,left);
+              curr[j][cost] = max(up,left);
              }
            }
+          prev = curr;
          }
 
-        if(dp[n-1][m-1][k]<0) return -1;
-        return dp[n-1][m-1][k];
+        if(prev[m-1][k]<0) return -1;
+        return prev[m-1][k];
     }
 };
