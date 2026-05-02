eb52d7d20a97f2853bb94c760240e4b354604b77
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-02 09:40:08 +0530

    Time: 47 ms (55.98%) | Memory: 13.1 MB (66.97%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPerfectSquare(int n)
+     {
+      int sqroot = (int) sqrt(n);
+      return (sqroot*sqroot == n);
+     }
+
+    int numSquares(int n) {
+       vector<int> dp(n+1,1e8);
+
+       dp[0] = 0;
+
+       for(int i=1; i<=n; i++)
+        {
+         for(int j=1; j*j<=i; j++)
+          {
+           int square = j*j;
+      
+           int notTake = dp[i];
+
+           int take = 1e8;
+           if(j<=i)
+               take = 1 + dp[i-square];
+
+           dp[i] = min(take,notTake);
+          }
+        }
+
+       return dp[n];
+    }
+};
