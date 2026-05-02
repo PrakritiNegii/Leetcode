88e4a32f8ad27a024771b516b8a833b4d398a119
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-02 17:03:31 +0530

    Time: 23 ms (70.22%) | Memory: 10.9 MB (71.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    int allWays(vector<int>& coins, int i, int amount, vector<vector<int>>& dp) 
-     {
-      if(amount==0) return 1;
-      if(i<0) return 0;
-      if(dp[i][amount]!=-1) return dp[i][amount];
-
-      int notTake = allWays(coins,i-1,amount,dp);
-
-      int take = 0;
-      if(amount>=coins[i])
-        take = allWays(coins,i,amount-coins[i],dp);
-
-      return dp[i][amount] = notTake + take;
-     }
     int change(int amount, vector<int>& coins) {
        int n = coins.size();
 
