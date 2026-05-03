663e5d82e75033575f424c050d8090428b6e582b
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-03 10:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.87%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int s1 = s.size();
-        int s2 = goal.size();
+      if(s.size()!=goal.size())  return false;
+      vector<int> start;
+      for(int i=0; i<s.size(); i++)
+       {
+        if(s[i]==goal[0]) start.push_back(i);
+       }  
 
-        if(s1!=s2) return false;
-
-        int i=0, j=0; //i for s and j for goal
-        while(i<s1 && j<s1)
-         {
-          if(s[i]!=goal[j]) 
-           {
-            j++;
-            continue;
-           } 
-          i++;  //i increases only when j matches with it
-          int k = (j+1)%s1;
-          while(k!=j)
-           {
-            if(goal[k]!=s[i])
-             {
-              break;
-             }
-            k = (k+1)%s1;
-            i++;
-           }
-          if(k==j) return true; //all of the string matched
-          i = 0;
-          j++;
-         }
-
-        return false;
+       for(int i=0; i<start.size(); i++)
+        {
+         int idx = (start[i]+1)%s.size();
+         int j = 1;
+         bool matches = true;
+         while(j<s.size())
+          {
+           if(goal[j]!=s[idx])
+            {
+             matches = false;
+             break;
+            }
+           j++;
+           idx = (idx+1)%s.size();
+          }
+         if(matches) return true;
+        }
+       return false;
     }
 };
