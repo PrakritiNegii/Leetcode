d48c2e0f415a05d091374f479965d40904699b25
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-04 11:48:14 +0530

    Time: 548 ms (92.62%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 DELETE FROM Person
-WHERE id NOT IN (
-    SELECT id FROM (
+WHERE id NOT IN (       #runs per row (just a simple check, not a re-running query)
+    SELECT id FROM (    #runs only once
         SELECT min(id) AS id
             FROM Person
             GROUP BY email) 
