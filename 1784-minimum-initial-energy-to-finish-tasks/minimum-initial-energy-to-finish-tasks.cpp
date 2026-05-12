5c28491d0f960a360c2d95927ba92d2515d31e64
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-12 22:49:21 +0530

    Time: 40 ms (56.80%) | Memory: 97.4 MB (39.81%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b)
+      {
+        return (a[1] - a[0]) > (b[1] - b[0]);
+      }
+    int minimumEffort(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+        sort(tasks.begin(),tasks.end(),comp);
+
+        int total = tasks[0][1];
+        int totalActual = tasks[0][0];
+
+        int curr = total - totalActual;
+        for(int i=1; i<n; i++)
+         {
+          int req = tasks[i][1] - curr;
+          if(req>0) total += req;
+
+          totalActual += tasks[i][0];
+          curr = total - totalActual;
+         } 
+
+        return total;
+    }
+};
