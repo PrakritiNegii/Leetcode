d8c541a29f9bda8e20ec375bb4135274859b0afc
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-16 12:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (63.69%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0, right = nums.size()-1;
-        int minimum = nums[0];
+        int n = nums.size();
 
-        while(left<=right)
+        int left = 0, right = n-1;
+
+        while(left<right)
          {
           int mid = (right-left)/2 + left;
 
-          if(nums[left]==nums[mid] && nums[mid]==nums[right])
+          if(nums[mid]>nums[right]) //left half is sorted
            {
-            left++;
-            right--;
-            minimum = min(minimum,nums[mid]);
-            continue;
+            left = mid + 1;
            }
-
-          if(nums[left]<=nums[mid]) //left half is sorted
-           {
-            minimum = min(minimum,nums[left]);
-            cout<<minimum<<endl;
-            //move towards the right
-            left = mid+1;
-           }
-          else
-           {
-            minimum = min(minimum,nums[mid]);
-            //move towards the left
-            right = mid-1;
-           }
+          else if(nums[mid]<nums[right]) //right half is sorted
+                {
+                 right = mid;
+                }
+               else
+                  {
+                    right--;
+                  }
          }
 
-        return minimum;
+        return nums[left]; //or nums[right] if left==right
     }
 };
