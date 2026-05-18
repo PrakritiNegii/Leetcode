2f0612e778a4736dc20d59ea5ecc0f598b4ef324
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-18 18:38:30 +0530

    Time: 191 ms (13.67%) | Memory: 111.9 MB (13.44%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,vector<int>> mp;
+
+        for(int i=0; i<n; i++)
+         {
+          mp[arr[i]].push_back(i);
+         }
+
+        // for(auto val : mp)
+        //  {
+        //   cout<<endl<<val.first<<endl;
+        //   cout<<"array: ";
+        //   auto a = val.second;
+        //   for(int j=0; j<a.size(); j++)
+        //    {
+        //     cout<<a[j]<<"\t";
+        //    }
+        //  }
+
+        queue<pair<int,int>> q; //index and step
+        q.push({0,0});
+
+        vector<bool> visited(n,false); 
+        visited[0] = true;
+
+        int minStep = n-1;
+
+        while(!q.empty())
+         {
+          int idx = q.front().first;
+          int step = q.front().second;
+          q.pop();
+
+        //   cout<<idx<<"\t"<<step<<endl;
+
+          if(idx==n-1) minStep = min(minStep,step);
+
+          if(idx-1>=0 && !visited[idx-1]) 
+            {
+             q.push({idx-1,step+1});
+             visited[idx-1] = true;
+            }
+          if(idx+1<n && visited[idx+1]==false) 
+            {
+             q.push({idx+1,step+1});
+             visited[idx+1] = true;
+            }
+
+          auto a = mp[arr[idx]];
+          for(int j=0; j<a.size(); j++)
+           {
+            // cout<<"array: "<<a[j]<<"\t";
+            if(idx==arr[j]) continue;
+            if(!visited[a[j]])
+              {
+                q.push({a[j],step+1});
+                visited[a[j]] = true;
+              }
+           }
+          mp.erase(arr[idx]);
+         }
+
+       return minStep;
+    }
+};
