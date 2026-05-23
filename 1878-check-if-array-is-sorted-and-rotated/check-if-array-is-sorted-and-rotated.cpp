8e83894d45b4f59abd823533a84ffa40f4ca24e4
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-23 09:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (82.15%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int size = nums.size();
-        int points = 0;
-        int i=0, j=-1;
-        while(j!=0)
-         {
-          j=(i+1)%size;
-          if(nums[i]>nums[j]) //if there is a point where any number is greater than the next number (can also do it for lesser)
-          //In a sorted array with duplicates there should only be 1 or 0 such points
-           {
-            points++;
-           }
-          i = j;
-         }
-        if(points>1)
-          return false;
-        else 
-          return true;
+       int n = nums.size();
+       int drops = 0;
+
+       for(int i=1; i<n; i++)
+        {
+         if(nums[i-1]>nums[i]) drops++;
+         if(drops>1) return false;
+        }
+
+       if(nums[n-1]>nums[0]) drops++;
+       if(drops>1) return false;
+
+       return true;
     }
 };
