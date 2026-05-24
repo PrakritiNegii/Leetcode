73f363712707d53bba7785dfa79322cfcfd814cc
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-24 15:30:34 +0530

    Time: 11 ms (74.31%) | Memory: 19.1 MB (38.23%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int dfs(vector<int>& arr, int d, int idx, vector<int>& dp)
      {
+        if(dp[idx] != 0)
+           return dp[idx];
         int n = arr.size();
         int maxIdx = 1;
         for(int k=idx+1; k<n && k<=idx+d; k++)
@@ -35,62 +37,9 @@ public:
 
         for(int i=0; i<n; i++)
          {
+
           maxVisits = max(maxVisits,dfs(arr,d,i,dp));
          }
-        // vector<bool> visited(n,false);
-        // vector<int> moves(n,0);
-        // int maxIdx = 1;
-
-        // for(int i=0; i<n; i++)
-        //  {
-        //   if(visited[i]==true) continue;
-        //   queue<pair<int,int>> q;
-        //   vector<bool> v(n,0);
-        //   q.push({i,1});
-        //   visited[i] = true;
-        //   v[i] = true;
-
-        //   int moves_i = 1;
-        //   while(!q.empty())
-        //    {
-        //     int idx = q.front().first;
-        //     int step = q.front().second;
-        //     maxIdx = max(maxIdx,step);
-        //     moves_i = max(moves_i,step);
-        //     q.pop();
-
-        //     for(int k=idx+1; k<n && k<=idx+d; k++)
-        //       {
-        //        if(arr[k]>=arr[idx]) break;
-        //        if(v[k]==true) continue;
-        //        visited[k] = true;
-        //        v[k] = true;
-        //        if(moves[k]!=0)
-        //         {
-        //          maxIdx = max(maxIdx,step+moves[k]);
-        //          moves_i = max(moves_i, step + moves[k]);
-        //          continue;
-        //         }
-        //        q.push({k,step+1});
-        //       }
-
-        //     for(int k=idx-1; k>=0 && k>=idx-d; k--)
-        //       {
-        //        if(arr[k]>=arr[idx]) break;
-        //        if(v[k]==true) continue;
-        //        visited[k] = true;
-        //        v[k] = true;
-        //        if(moves[k]!=0)
-        //         {
-        //          maxIdx = max(maxIdx,step+moves[k]);
-        //          moves_i = max(moves_i, step + moves[k]);
-        //          continue;
-        //         }
-        //        q.push({k,step+1});
-        //       }
-        //     }
-        //    moves[i] = moves_i;
-        //  }
 
         return maxVisits;
     }
