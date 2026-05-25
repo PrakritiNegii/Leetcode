f7955468555fb4233a260d8e572e10352d336328
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-05-25 19:44:05 +0530

    Time: 700 ms (5.25%) | Memory: 23.9 MB (49.83%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        if(s[n-1]=='1') return false;
+
+        vector<bool> visited(n,false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+        int maxIdx = 0;
+
+        while(!q.empty()) 
+         {
+          int i = q.front();
+          q.pop();
+
+          for(int j=max(i+minJump,maxIdx); j<=min(i+maxJump,n-1); j++)
+           {
+            maxIdx = j;
+            if(visited[j]==true) continue;
+
+            if(s[j]=='1')
+             {
+              visited[j] = true;
+              continue;
+             } 
+            
+            if(j==n-1) return true;
+            q.push(j);
+            visited[j] = true;
+           }
+         }
+
+        for(int i=0; i<n; i++)
+          cout<<visited[i]<<endl;
+
+        return false;
+    }
+};
