1e4d5884a5daf4f4a0dd8b10ea7e6a4500715391
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-03 22:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 91.9 MB (60.66%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int l = landStartTime.size();
-        int w = waterStartTime.size();
+    int earliestFinishTime(vector<int>& lt, vector<int>& ld, vector<int>& wt, vector<int>& wd) {
+        int l = lt.size();
+        int w = wt.size();
         int time = INT_MAX;
+
+        int minL = INT_MAX;
+        for(int i=0; i<l; i++)
+         {
+          minL = min(minL,lt[i]+ld[i]);
+         }
+
+        int minW = INT_MAX;
+        for(int i=0; i<w; i++)
+         {
+          minW = min(minW,wt[i]+wd[i]);
+          time = min(time,max(wt[i],minL)+wd[i]); //min(time,minL+max(wt[i],minL)-minL+wd[i])
+         }
 
         for(int i=0; i<l; i++)
          {
-          int tt = landStartTime[i] + landDuration[i];
-          for(int j=0; j<w; j++)
-           {
-            int t = 0;
-            if(waterStartTime[j]>tt) t += waterStartTime[j] - tt;
-            t += waterDuration[j];
-
-            time = min(time,tt+t);
-           }
+          time = min(time,max(lt[i],minW)+ld[i]);
          }
 
-        for(int i=0; i<w; i++)
-         {
-          int tt = waterStartTime[i] + waterDuration[i];
-          for(int j=0; j<l; j++)
-           {
-            int t = 0;
-            if(landStartTime[j]>tt) t += landStartTime[j] - tt;
-            t += landDuration[j];
-
-            time = min(time,tt+t);
-           }
-         }
         return time;
     }
 };
