564ac46a5e3d425b10ce1170dfeb468c67c85617
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-04 19:14:12 +0530

    Time: 39 ms (28.22%) | Memory: 27.7 MB (13.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,40 +1,17 @@
 class Solution {
-  public:
-    int longestCommonSubsequence(string &s1, string &s2) {
-      int size1 = s1.size();
-      int size2 = s2.size();
+public:
+    int lcsbstr(string &s1, string &s2, int i, int j, vector<vector<int>>& dp)
+     {
+      if(i==0 || j==0) return 0;
+      if(dp[i][j]!=-1) return dp[i][j];
+      if(s1[i-1]==s2[j-1]) return dp[i][j] = 1+lcsbstr(s1,s2,i-1,j-1,dp);
       
-      vector<vector<int>> dp(size1,vector<int>(size2,0));
-      
-      if(s1[0]==s2[0]) dp[0][0] = 1;
-      
-      for(int j=1; j<size2; j++)
-       {
-        if(s1[0]==s2[j]) 
-            dp[0][j] = 1;
-        else 
-            dp[0][j] = dp[0][j-1];
-       }
-       
-      for(int i=1; i<size1; i++)
-       {
-        if(s1[i]==s2[0]) 
-            dp[i][0] = 1;
-        else 
-            dp[i][0] = dp[i-1][0];
-       }
-       
-      for(int i=1; i<size1; i++)
-       {
-        for(int j=1; j<size2; j++)
-         {
-          if(s1[i]==s2[j])
-            dp[i][j] = 1 + dp[i-1][j-1];
-          else
-            dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-         }
-       }
-       
-      return dp[size1-1][size2-1];
+      return dp[i][j] = max(lcsbstr(s1,s2,i-1,j,dp),lcsbstr(s1,s2,i,j-1,dp));
+     }
+     
+    int longestCommonSubsequence(string s1, string s2) {
+       int n1=s1.size(), n2=s2.size();
+       vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+       return lcsbstr(s1,s2,n1,n2,dp); 
     }
 };
