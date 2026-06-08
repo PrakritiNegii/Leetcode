37d9413b32ee492745adde5a96dcf7b6f1435850
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-08 08:33:51 +0530

    Time: 116 ms (92.44%) | Memory: 259.4 MB (88.87%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -1,75 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
         int n = descriptions.size();
-        unordered_set<int> child;
-        unordered_map<int,vector<pair<int,int>>> parent;
+        TreeNode* root;
+        unordered_map<int, TreeNode*> mp;
 
-        for(int i=0; i<n; i++)
-         {
-          int p = descriptions[i][0];
-          int c = descriptions[i][1];
-          int l = descriptions[i][2];
+        for (int i = 0; i < n; i++) {
+            int curr = descriptions[i][1];
+            mp[curr] = new TreeNode(curr);
+        }
 
-          child.insert(c);
-          parent[p].push_back({c,l});
-         }
+        for (int i = 0; i < n; i++) {
+            int parent = descriptions[i][0];
+            int curr = descriptions[i][1];
+            bool isLeft = descriptions[i][2];
 
-        int root;
-        for(int i=0; i<n; i++)
-         {
-          int p = descriptions[i][0];
-          
-          if(child.count(p)==0) //if the parent found is not anyones child then the parent found is the root node
-           {
-            root = p;
-            break;
-           }
-         }
+            // find root
+            if (mp.find(parent) == mp.end()) {
+                mp[parent] = new TreeNode(parent);
+                root = mp[parent];
+            }
 
-        TreeNode *rt =  new TreeNode();;
-        rt->val = root;
-        rt->left = NULL;
-        rt->right = NULL;
+            TreeNode* parentNode = mp[parent];
+            if (isLeft) {
+                parentNode->left = mp[curr];
+            } else {
+                parentNode->right = mp[curr];
+            }
 
-        queue<TreeNode*> q;
-        q.push(rt);
+            mp[parent] = parentNode;
+        }
 
-        while(!q.empty())
-         {
-          TreeNode *node = q.front();
-          q.pop();
-
-          auto arr = parent[node->val];
-          for(int i=0; i<arr.size(); i++)
-           {
-            int v = arr[i].first;
-            int l = arr[i].second;
-            TreeNode *t =  new TreeNode();
-            t->val = v;
-            t->left = NULL;
-            t->right = NULL;
-
-            q.push(t);
-
-            if(l==1) 
-              node->left = t;
-            else 
-              node->right = t;
-           }
-         }
-
-       return rt;
+        return root;
     }
 };
