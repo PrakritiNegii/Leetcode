9aad013f527c95ad8de8e0fac0a4d9ce49167d55
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-11 12:09:12 +0530

    Time: 93 ms (27.38%) | Memory: 76 MB (21.28%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -14,13 +14,9 @@ public:
           for(int j=1; j<=n; j++)
            {
             if(s[i-1]==rev[j-1])
-             {
               dp[i][j] = 1+dp[i-1][j-1];
-             }
             else
-             {
               dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-             }
            }
          }
 
