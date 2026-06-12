6a006be6751dc77c1ef3ca9c85825f947e71f867
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-12 10:08:40 +0530

    Time: 23 ms (88.06%) | Memory: 26.7 MB (69.59%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i=1; i<=n; i++)
+         {
+          for(int j=1; j<=m; j++)
+           {
+            if(str1[i-1]==str2[j-1])
+              dp[i][j] = 1 + dp[i-1][j-1];
+            else
+              dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+           }
+         }
+
+        string scs = "";
+        int i=n, j=m;
+        while(i>0 && j>0)
+         {
+          if(str1[i-1]==str2[j-1])
+           {
+            scs += str1[i-1];
+            i--; j--;
+           }
+          else
+           {
+            if(dp[i-1][j]>=dp[i][j-1])
+             {
+               scs += str1[i-1]; //leaving str1[i-1] so add it
+               i--; //move upwards
+             } 
+            else 
+              {
+                scs += str2[j-1]; //leaving str2[j] so add it
+                j--; //move leftwards
+              }
+           }
+         }
+
+        while(i>0)
+         {
+          scs += str1[i-1];
+          i--;
+         }
+
+        while(j>0)
+         {
+          scs += str2[j-1];
+          j--;
+         }
+
+        reverse(scs.begin(),scs.end());
+
+        return scs;
+    }
+};
