0ec52e251bc4dab3ca1389d5856bf862ebcf818e
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-13 13:09:02 +0530

    Time: 31 ms (51.31%) | Memory: 44.2 MB (32.21%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    long long distincts(string& s, int j, string& t, int i, vector<vector<long long>>& dp)
-     {
-      if(i==0) return 1;
-      if(j==0) return 0;
-      if(dp[i][j]!=-1) return dp[i][j];
-
-      long long take=0, notTake=0;
-      if(s[j-1]==t[i-1])
-        take = distincts(s,j-1,t,i-1,dp);
-      
-      notTake = distincts(s,j-1,t,i,dp);
-
-      return dp[i][j] = take + notTake;
-     }
-
     int numDistinct(string s, string t) {
         int m=s.size(), n=t.size();
 
-        vector<vector<long long>> dp(n+1,vector<long long>(m+1,-1)); //shorter string for row
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0)); //shorter string for row
 
-        return (int) distincts(s,m,t,n,dp);
+        for(int j=0; j<=m; j++)
+          dp[0][j] = 1;
+
+        for(int i=1; i<=n; i++)
+         {
+            for(int j=1; j<=m; j++)
+              {
+                unsigned long long take = 0;
+                if(s[j-1]==t[i-1]) 
+                   take = dp[i-1][j-1];
+                
+                unsigned long long notTake = dp[i][j-1];
+
+                dp[i][j] = take + notTake;
+              }
+         }
+
+        return (int) dp[n][m];
     }
 };
