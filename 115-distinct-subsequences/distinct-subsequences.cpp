06575e7a34cb7b8d348dc36a6a9f568ed0cf1c09
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-13 13:17:11 +0530

    Time: 23 ms (75.40%) | Memory: 26.8 MB (73.83%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int n=s.size(), m=t.size();
+        int m=s.size(), n=t.size();
 
-        vector<unsigned long long> prev(m+1,0), curr(m+1,0); //shorter string for row
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0)); //shorter string for row
 
-        prev[0] = 1; curr[0] = 1;
-        for(int i=1; i<=n; i++)
-         {
-            for(int j=1; j<=m; j++)
+        int totali = 0;
+        for(int j=1; j<=m; j++) //for row 1
+          {
+            if(t[0]==s[j-1])
               {
-                unsigned long long take = 0;
-                if(s[i-1]==t[j-1]) 
-                   take = prev[j-1];
-                
-                unsigned long long notTake = prev[j];
-
-                curr[j] = take + notTake;
+                dp[1][j] = 1; 
+                totali += dp[1][j];
               }
-            prev = curr;
-         }
+          }
 
-        return (int) prev[m];
+        for(int i=2; i<=n; i++)
+         {
+          char lasti = t[i-2];
+          long long lastC = 0;
+          for(int j=1; j<=m; j++)
+           {
+            if(t[i-1]==s[j-1]) 
+              {
+                dp[i][j] = lastC;
+              }
+            if(lasti==s[j-1])
+              {
+                lastC += 1LL * dp[i-1][j];
+              }
+           }
+         }
+         
+        long long total = 0; //the sum of last row
+        for(int j=1; j<=m; j++)
+          total += 1LL * dp[n][j];
+
+        return (int) total;
     }
 };
