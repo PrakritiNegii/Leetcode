dd837469f24abc03e81f0e4a564da810d39a987e
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-13 13:12:45 +0530

    Time: 31 ms (51.31%) | Memory: 44.4 MB (21.05%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int m=s.size(), n=t.size();
+        int n=s.size(), m=t.size();
 
         vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0)); //shorter string for row
 
-        for(int j=0; j<=m; j++)
-          dp[0][j] = 1;
+        for(int i=0; i<=n; i++)
+          dp[i][0] = 1;
 
         for(int i=1; i<=n; i++)
          {
             for(int j=1; j<=m; j++)
               {
                 unsigned long long take = 0;
-                if(s[j-1]==t[i-1]) 
+                if(s[i-1]==t[j-1]) 
                    take = dp[i-1][j-1];
                 
-                unsigned long long notTake = dp[i][j-1];
+                unsigned long long notTake = dp[i-1][j];
 
                 dp[i][j] = take + notTake;
               }
