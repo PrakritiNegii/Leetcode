30b3c9afb5d5c85a005699f822a1286814c81c52
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-14 18:07:41 +0530

    Time: 23 ms (14.48%) | Memory: 14.3 MB (16.46%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minChanges(string &w1, int i, string &w2, int j, vector<vector<int>>& dp)
+     {
+      if(i==w1.size() && j==w2.size()) return 0;
+      if(i==w1.size()) return w2.size()-j;
+      if(j==w2.size()) return w1.size()-i;
+
+      if(dp[i][j]!=-1) return dp[i][j];
+
+      int match = 1e8;
+      if(w1[i]==w2[j]) match = minChanges(w1,i+1,w2,j+1,dp);
+     
+      int rem = 1 + minChanges(w1,i+1,w2,j,dp);
+      int ins = 1 + minChanges(w1,i,w2,j+1,dp);
+      int rep = 1 + minChanges(w1,i+1,w2,j+1,dp);
+
+      return dp[i][j] = min({match,rem,ins,rep});
+     }
+
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return minChanges(word1,0,word2,0,dp);
+    }
+};
