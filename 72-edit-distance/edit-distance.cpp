222244fe8fad592d8175fab59e6ef77d471793af
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-14 18:18:55 +0530

    Time: 1578 ms (5.38%) | Memory: 14.6 MB (12.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minChanges(string &w1, int i, string &w2, int j, vector<vector<int>>& dp)
      {
-      if(i==w1.size() && j==w2.size()) return 0;
-      if(i==w1.size()) return w2.size()-j;
-      if(j==w2.size()) return w1.size()-i;
+      cout<<i<<"\t"<<j<<endl;
+      if(i==0 && j==0) return 0;
+      if(i==0) return j;
+      if(j==0) return i;
 
       if(dp[i][j]!=-1) return dp[i][j];
 
-      if(w1[i]==w2[j]) return minChanges(w1,i+1,w2,j+1,dp);
+      if(w1[i-1]==w2[j-1]) return minChanges(w1,i-1,w2,j-1,dp);
      
-      int rem = 1 + minChanges(w1,i+1,w2,j,dp);
-      int ins = 1 + minChanges(w1,i,w2,j+1,dp);
-      int rep = 1 + minChanges(w1,i+1,w2,j+1,dp);
+      int rem = 1 + minChanges(w1,i-1,w2,j,dp);
+      int ins = 1 + minChanges(w1,i,w2,j-1,dp);
+      int rep = 1 + minChanges(w1,i-1,w2,j-1,dp);
 
       return dp[i][j] = min({rem,ins,rep});
      }
@@ -20,7 +21,7 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size(), m = word2.size();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return minChanges(word1,0,word2,0,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return minChanges(word1,n,word2,m,dp);
     }
 };
