a6ea6e639313a6a50a20025d8f1d746463a1da2f
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-18 13:31:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (80.18%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    bool doesMatch(string s, int i, string p, int j, vector<vector<int>>& dp)
-     {
-      if(i<=0 && j<=0) return true;
-      if(j<=0) return false;
-
-      if(i>0 && dp[i][j]!=-1) return dp[i][j];
-
-      if(i>0 && (s[i-1]==p[j-1] || p[j-1]=='.'))
-        return dp[i][j] = doesMatch(s,i-1,p,j-1,dp);
-
-      if(p[j-1]=='*') 
-       {
-        int notTake = doesMatch(s,i,p,j-2,dp); //matching value before * with nothing
-        int take = false;
-        if(i>0 && (p[j-2]=='.' || p[j-2]==s[i-1])) take = doesMatch(s,i-1,p,j,dp)/*one match*/;
-
-        return dp[i][j] = notTake | take;
-       }
-      return dp[i][j] = false;
-     }
-
     bool isMatch(string s, string p) {
        int n = s.size(), m = p.size();
 
-       vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-       bool matching = doesMatch(s,n,p,m,dp); 
-       return matching;
+       vector<bool> prev(m+1,0), curr(m+1,0);
+
+       prev[0] = true;  //prev here is i=0;
+
+       curr[0] = true; //curr here is also i = 0;
+
+       for(int i=0; i<=n; i++)
+        {
+         if(i>0) curr[0] = false;
+         for(int j=1; j<=m; j++)
+          {
+           if(i>0 && (s[i-1]==p[j-1] || p[j-1]=='.')) 
+              curr[j] = prev[j-1];
+           else if(p[j-1]=='*')
+                 {
+                   int notTake = curr[j-2]; //matching value before * with nothing
+                   int take = false;
+                   if(i>0 && (p[j-2]=='.' || p[j-2]==s[i-1])) 
+                       take = prev[j]/*one match*/;
+                   curr[j] = notTake | take;
+                 }
+                else 
+                   curr[j] = false;
+          }
+         prev = curr;
+        }
+
+       return prev[m];
     }
 };
