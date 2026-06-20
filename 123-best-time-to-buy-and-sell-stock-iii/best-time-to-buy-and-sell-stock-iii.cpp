5618089af01621ddcae769ad521553b63c0b26f6
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-20 09:11:21 +0530

    Time: 56 ms (62.24%) | Memory: 79.5 MB (70.38%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+
+        vector<vector<int>> next(2,vector<int>(3,0)), curr(2,vector<int>(3,0));
+
         for(int i=n-1; i>=0; i--)
          {
           for(int bought=0; bought<=1; bought++)
@@ -12,18 +14,19 @@ public:
               int take=0, notTake=0;
               if(!bought) // not bought then we can buy
                {
-                take = -prices[i] + dp[i+1][true][count];
-                notTake = dp[i+1][false][count];
+                take = -prices[i] + next[true][count];
+                notTake = next[false][count];
                }
               else //bought then we sell
                {
-                take = prices[i] + dp[i+1][false][count+1];  
-                notTake = dp[i+1][true][count];  
+                take = prices[i] + next[false][count+1];  
+                notTake = next[true][count];  
                }
-              dp[i][bought][count] = max(take,notTake);
+              curr[bought][count] = max(take,notTake);
              }
            }
+          next = curr;
          }
-        return dp[0][false][0];
+        return next[false][0];
     }
 };
