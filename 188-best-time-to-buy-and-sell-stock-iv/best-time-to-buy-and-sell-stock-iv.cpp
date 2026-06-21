070391a39a01555aec9c2ce1dbe63b2fd03be24a
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-21 08:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (79.68%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int mProfit(vector<int>& prices, int i, int j, vector<vector<int>>& dp)
-     {
-      if(i==prices.size() || j==dp[0].size()) return 0;
-
-      if(dp[i][j]!=-1) return dp[i][j];
-
-      int take=0, notTake=0;
-      if(j%2==0)
-       {
-        take = -prices[i] + mProfit(prices,i+1,j+1,dp);
-        notTake = mProfit(prices,i+1,j,dp);
-       }
-      else
-       {
-        take = prices[i] + mProfit(prices,i+1,j+1,dp);
-        notTake = mProfit(prices,i+1,j,dp);
-       }
-
-      return dp[i][j] = max(take,notTake);
-     }
-
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
+        vector<int> next(k*2+1,0), curr(k*2+1,0);
 
-        vector<vector<int>> dp(n,vector<int>(k*2,-1));
+        for(int i=n-1; i>=0; i--)
+         {
+          for(int j=k*2-1; j>=0; j--)
+           {
+            int take = 0, notTake = 0;
+            if(j%2==0)
+             {
+              take = -prices[i] + next[j+1];
+              notTake = next[j];
+             }
+            else
+             {
+              take = prices[i] + next[j+1];
+              notTake = next[j];
+             }
+            curr[j] = max(take,notTake);
+           }
+          next = curr;
+         }
 
-        return mProfit(prices,0,0,dp);
+        return next[0];
     }
 };
