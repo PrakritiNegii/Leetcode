28385530cc2def97b3960106e32133035635e856
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-06-21 08:15:14 +0530

    Time: 14 ms (32.19%) | Memory: 17.9 MB (52.58%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int mProfit(vector<int>& prices, int i, bool bought, int k, vector<vector<vector<int>>>& dp)
+    int mProfit(vector<int>& prices, int i, int j, vector<vector<int>>& dp)
      {
-      if(i==prices.size() || k<0) return 0;
+      if(i==prices.size() || j==dp[0].size()) return 0;
 
-      if(dp[i][bought][k]!=-1) return dp[i][bought][k];
+      if(dp[i][j]!=-1) return dp[i][j];
 
       int take=0, notTake=0;
-      if(!bought)
+      if(j%2==0)
        {
-        take = -prices[i] + mProfit(prices,i+1,true,k,dp);
-        notTake = mProfit(prices,i+1,false,k,dp);
+        take = -prices[i] + mProfit(prices,i+1,j+1,dp);
+        notTake = mProfit(prices,i+1,j,dp);
        }
       else
        {
-        take = prices[i] + mProfit(prices,i+1,false,k-1,dp);
-        notTake = mProfit(prices,i+1,true,k,dp);
+        take = prices[i] + mProfit(prices,i+1,j+1,dp);
+        notTake = mProfit(prices,i+1,j,dp);
        }
 
-      return dp[i][bought][k] = max(take,notTake);
+      return dp[i][j] = max(take,notTake);
      }
 
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k,-1)));
+        vector<vector<int>> dp(n,vector<int>(k*2,-1));
 
-        return mProfit(prices,0,false,k-1,dp);
+        return mProfit(prices,0,0,dp);
     }
 };
