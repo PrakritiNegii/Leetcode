54c6bb21f821d3b7054d1e253eefbec88aa0a16e
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-07-31 22:46:33 +0530

    Time: 29 ms (94.83%) | Memory: 19.1 MB (53.68%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),[](const string &a, const string &b)
+         {
+          return a.length() < b.size();
+         });
+         /*
+         Use <, not <=
+         std::sort requires the comparator to define a strict weak ordering.
+
+That means:
+
+cmp(a, a) must always be false.
+If cmp(a, b) is true, then cmp(b, a) must be false.
+
+
+         */
+
+        int n = words.size();
+
+        vector<int> lis(n,1);
+        vector<int> hash(n);
+        int maxLis = 1;
+        int maxIdx = 0;
+        for(int i=0; i<n; i++)
+         {
+          hash[i] = i;
+          for(int j=0; j<i; j++)
+           {
+            if(words[i].size()-1 != words[j].size()) 
+              continue;
+            string a = words[i];
+            string b = words[j];
+            int difference = 0;
+            int x=0, y=0;
+            while(x<a.size() && difference<=1)
+             {
+              if(y==b.size() || a[x]!=b[y])
+               {
+                difference++;
+               }
+              else y++;
+              x++;
+             }
+            if(difference==1)
+             {
+              if(lis[i]<lis[j]+1)
+               {
+                lis[i] = lis[j]+1;
+                hash[i] = j;
+               }
+             }
+           }
+          if(maxLis<lis[i])
+           {
+            maxLis = lis[i];
+            maxIdx = i;
+           }
+         }
+
+      return maxLis;
+    }
+};
