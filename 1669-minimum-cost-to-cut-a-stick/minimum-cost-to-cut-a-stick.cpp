1267dffe033b540740ca1cfde89ecf5a04fc9d8a
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2026-08-16 22:53:01 +0530

    Time: 663 ms (5.18%) | Memory: 26.3 MB (5.41%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int partition(vector<int>& cuts, int i, int j, vector<bool> &visited, map<pair<int,int>,int>& dp)
+     {
+      if(i==j-1) return 0;
+
+      if(dp.count({i,j})!=0) return dp[{i,j}];
+
+      int minCost = INT_MAX;
+      for(int c=0; c<cuts.size(); c++)
+       {
+        if(cuts[c]<i || cuts[c]>j) continue;
+
+        int cost = INT_MAX;
+        if(visited[c]==0)
+         {
+          visited[c] = 1;
+
+          cost = (j-i) + partition(cuts,i,cuts[c],visited,dp) + partition(cuts,cuts[c],j,visited,dp);
+
+          visited[c] = 0;
+         }
+
+        minCost = min(minCost,cost);
+       }  
+      if(minCost==INT_MAX) return dp[{i,j}] = 0;
+      return dp[{i,j}] = minCost;
+     }
+    
+    int minCost(int n, vector<int>& cuts) {
+        int cutsSize = cuts.size();
+
+        vector<bool> visited(cutsSize,0);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        map<pair<int,int>,int> dp;
+        return partition(cuts,0,n,visited,dp);
+    }
+};
